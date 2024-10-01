6a93441964e2e8db3b81d1ca04d6d17dfb523d77
Hiromi0613 <bearbook0613@gmail.com>
2024-10-01 10:59:23 +0900

    Update Problem1classItem.java

--- Test5/src/test5/Problem1classItem.java
+++ Test5/src/test5/Problem1classItem.java
@@ -1,25 +1,51 @@
 package test5;
 
 /**
- * @getTotalPrice 金額から税込み価格を計算
+ * 商品名と金額の引数を受け取り、金額に消費税を足し、結果を元のクラスに戻す
  */
 class Item {
-	// @param Name 商品名
+	/**
+	 *  @param Name 商品名
+	 */
 	public String Name;
-	// @param price 金額
-	public double price;
-	// @param tax　消費税率（１０％）
+	/**
+	 * @param price 金額
+	 */
+	public int price;
+	/**
+	 * @param tax　消費税率（１０％）
+	 */
 	public final double tax = 0.1;
-	
+
+	/**
+	 * setNumメソッド
+	 * 受け取ったString型の引数nを変数Nameに代入する
+	 * @param n　商品名の引数
+	 */
 	public void setName(String n) {
 		this.Name = n;
 	}
-	public void setPrice(double p) {
+	/**
+	 * setPriceメソッド
+	 * 受け取ったint型の引数pを変数priceに代入する
+	 * @param p　金額の引数
+	 */
+	public void setPrice(int p) {
 		this.price = p;
 	}
+	/**
+	 * getItemNameメソッド
+	 * String型の変数Nameを返す
+	 * @return　変数Name
+	 */
 	public String getItemName() {
 		return Name;
 	}
+	/**
+	 * getTotalPriceメソッド
+	 * 金額に消費税を足して返す
+	 * @return　税込み価格
+	 */
 	public double getTotalPrice() {
 		return price + price * tax;
 	}
