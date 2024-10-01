8542c3dc0d23a17236f5872e0002bcc900538b97
Hiromi0613 <bearbook0613@gmail.com>
2024-10-01 15:53:03 +0900

    Update Problem2classInf.java

--- Test6/src/test6/Problem2classInf.java
+++ Test6/src/test6/Problem2classInf.java
@@ -1,47 +1,104 @@
 package test6;
 
+/**
+ * 名前、年齢、住所、電話番号の引数を受け取り、元のクラスに戻す
+ */
 class Inf {														
-	//@param name 名前														
+	/** 名前 */ 														
 	private String name;														
-	//@param age 年齢														
+	/** 年齢 */														
 	private int age;														
-	//@param address 住所														
+	/** 住所 */														
 	private String address;														
-	//@param telnum 電話番号														
+	/** 電話番号 */														
 	private String telnum;	
 	
-	//初期化する														
+	/** 初期化する */
 	public Inf() {
 		name = null;														
 		age = 0;														
 		address = null;														
 		telnum = null;														
-	}														
+	}
+
+	/**
+	 * setNameメソッド
+	 * 受け取った名前の引数を変数nameに代入する
+	 * @param n 名前の引数
+	 */
 	public void setName(String n) {														
 		this.name = n;														
-	}														
+	}
+
+	/**
+	 * setAgeメソッド
+	 * 受け取ったint型の年齢の引数を変数ageに代入する
+	 * @param ai 年齢の引数
+	 */
 	public void setAge(int ai) {														
 		this.age = ai;														
-	}														
+	}
+
+　　　　/**
+	 * setAgeメソッド
+	 * 受け取ったstring型の年齢の引数を変数ageに代入する
+	 * @param as 年齢の引数
+	 */														
 	public void setAge(String as) {														
 		int agei = Integer.parseInt(as);														
 		this.age = agei;														
-	}														
+	}	
+
+	/**
+	 * setAddressメソッド
+	 * 受け取った住所の引数を変数Addressに代入する
+	 * @param ad 住所の引数
+	 */												
 	public void setAddress(String ad) {														
 		this.address = ad;														
-	}														
+	}	
+
+	/**
+	 * setTelnumメソッド
+	 * 受け取った電話番号の引数を変数telnumに代入する
+	 * @param t 電話番号の引数
+	 */													
 	public void setTelnum(String t) {														
 		this.telnum = t;														
-	}														
+	}	
+
+	/**
+	 * getNameメソッド
+	 * String型の変数nameを返す
+	 * @return　変数name
+	 */												
 	public String getName() {														
 		return name;														
-	}														
+	}
+
+	/**
+	 * getAgeメソッド
+	 * int型の変数ageを返す
+	 * @return　変数age
+	 */													
 	public int getAge() {														
 		return age;														
-	}														
+	}		
+
+	/**
+	 * getAddressメソッド
+	 * String型の変数addressを返す
+	 * @return　変数address
+	 */											
 	public String getAddress() {														
 		return address;														
-	}														
+	}
+
+	/**
+	 * getTelnumメソッド
+	 * String型の変数telnumを返す
+	 * @return　変数telnum
+	 */																										
 	public String getTelnum() {														
 		return telnum;														
 	}														
