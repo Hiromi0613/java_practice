2a7494610fee2c6121d475b8a6d47e27a9d5668a
Hiromi0613 <bearbook0613@gmail.com>
2024-10-01 11:03:46 +0900

    Update Problem2classCalculation.java

--- Test5/src/test5/Problem2classCalculation.java
+++ Test5/src/test5/Problem2classCalculation.java
@@ -1,29 +1,56 @@
 package test5;
 
 /**
- * @param result num1とnum2の計算結果
+ * 受けとった２つの数値と演算子から計算結果を出力する
  */
 class Calculation {
-	// @param num1 入力した一つ目の数値
+	/**
+	 *  @param num1 入力した一つ目の数値
+	 */
 	public double num1;
-	// @param num2 入力した二つ目の数値
+	/**
+	 *  @param num2 入力した二つ目の数値
+	 */
 	public double num2;
-	// @param ope 演算子
+	/**
+	 *  @param ope 演算子
+	 */
 	public String ope;
-	
-	public void setNum(double n1, double n2) {
+
+	/**
+	 * setNum1メソッド
+	 * 一つ目の数値の引数を変数num1に代入する
+	 * @param n1 一つ目の数値の引数
+	 */
+	public void setNum1(double n1) {
 		this.num1 = n1;
+	}
+	/**
+	 * setNum2メソッド
+	 * 二つ目の数値の引数を変数num2に代入する
+	 * @param n2　一つ目の数値の引数
+	 */
+	public void setNum2(double n2) {
 		this.num2 = n2;
 	}
+	/**
+	 * setOperatorメソッド
+	 * 演算子の引数を変数oに代入する
+	 * @param o　演算子の引数
+	 */
 	public void setOperator(String o) {
 		this.ope = o;
 	}
+	/**
+	 * getReaultメソッド
+	 * 演算子に沿った計算をし、結果を出力する
+	 */
 	public void getResult() {
 		double result = 0;
 		switch(ope) {
 		    case"+":
-			    result = num1 + num2;
-			    break;
+		        result = num1 + num2;
+		        break;
 		    case"-":
 		    	result = num1 - num2;
 		    	break;
@@ -42,5 +69,4 @@ class Calculation {
 	}
 		System.out.println("計算結果：" + result);
 	}
-	
 }
