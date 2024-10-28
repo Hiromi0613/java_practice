86699ccbef69c84d2280038c1098076cb8d64c1a
User <User@192.168.1.24>
2024-10-28 22:38:19 +0900

    修正

--- Lesson9/src/lesson9/ProcessA.java
+++ Lesson9/src/lesson9/ProcessA.java
@@ -16,6 +16,7 @@ public class ProcessA implements Process {
 	 * name,ageが条件に合うか判断する
 	 * @param name 名前
 	 * @param age 年齢
+	 * @return boolean型の戻り値を返す
 	 */
 	public boolean check(String name, int age) {
 		this.name = name;
